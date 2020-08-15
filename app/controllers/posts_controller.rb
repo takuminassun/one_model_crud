b16e381892a6810cb7bb948622c7c2c25c20edb4
takuminassun <nasutaku7nasuka@docomo.ne.jp>
2020-08-15 15:04:51 +0900

    post new create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,17 @@
 class PostsController < ApplicationController
-  def index
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.permit(:title, :content)
   end
 end
